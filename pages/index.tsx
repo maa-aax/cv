39ffb9d9320417ea6409fd8b366086b7357e018b
maa-aax <maxime.duhamel@protonmail.com>
2026-03-04 20:18:17 +0100

    latin

--- pages/index.tsx
+++ pages/index.tsx
@@ -372,7 +372,7 @@ export default function Home() {
                     </span>
                   </div>
                   <p className="mt-1 text-justify">
-                    
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                   </p>
                   <div className="flex flex-wrap mt-2 space-x-1 space-y-1 space-x-reverse">
                     <Tag color="red" classes="mt-1 mr-1">{skills.js}</Tag>
